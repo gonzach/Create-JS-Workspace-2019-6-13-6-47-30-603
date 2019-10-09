676f641e92e05dd82c0e690e99c81aa52c06d426
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-09 09:19:04 +0800

    Git ignore

--- js-workspace/__test__/index.spec.js
+++
@@ -1,5 +0,0 @@
-const add = require('../index');
-
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-});
--- js-workspace/__test__/main-spec.js
+++ js-workspace/__test__/main-spec.js
@@ -1,4 +1,4 @@
-const add = require('../main');
+const add = require('../main-spec');
 
 it ('should add two numbers', () => {
     expect(add(2, 3)).toBe(5);
